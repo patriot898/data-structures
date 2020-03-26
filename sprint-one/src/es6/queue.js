964ccc8751fe0997b2c5b9d783fab2509c7331bf
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-03-25 21:29:05 -0700

    Complete ES6 stack.js and queue.js

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,52 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    //this.enqueue = enqueue;
+    //this.dequeue = dequeue;
+    //this.size = size;
   }
+  enqueue(value) {
+    this.storage[this.size()] = value;
+  };
+
+  dequeue() {
+    var dequeued = this.storage[0];
+    for(var key in this.storage) {
+      this.storage[key] = this.storage[parseInt(key) + 1];
+    }
+    delete this.storage[this.size() - 1];
+    return dequeued;
+  };
+
+  size() {
+    var sizeCounter = 0;
+    for(var key in this.storage) {
+      sizeCounter++
+    }
+    return sizeCounter;
+  };
 
 }
+
+
+  /*var enqueue = function(value) {
+      this.storage[this.size()] = value;
+    };
+
+  var dequeue = function() {
+      var dequeued = this.storage[0];
+      for(var key in this.storage) {
+        this.storage[key] = this.storage[parseInt(key) + 1];
+      }
+      delete this.storage[this.size() - 1];
+      return dequeued;
+    };
+
+  var size = function() {
+      var sizeCounter = 0;
+      for(var key in this.storage) {
+        sizeCounter++
+      }
+      return sizeCounter;
+    };*/
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,24 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  push(value) {
+    this.storage[this.size()] = value;
+  };
+
+  pop() {
+    var popped = this.storage[this.size() - 1];
+    delete this.storage[this.size() - 1];
+    return popped;
+  };
+
+  size() {
+    var sizeCounter = 0;
+    for(var key in this.storage) {
+      sizeCounter++;
+    }
+    return sizeCounter;
+  }
 }
